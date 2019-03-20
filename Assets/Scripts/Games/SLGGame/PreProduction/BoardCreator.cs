f9e54bb28503bbbe64d1984c9023021596f74814
CrazyVulture <gaochen010@sina.com>
2019-03-20 18:00:32 +0800

    add board creator to generate tile maps

---
+++ Assets/Scripts/Games/SLGGame/PreProduction/BoardCreator.cs
@@ -0,0 +1,179 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+
+public class BoardCreator : MonoBehaviour
+{
+    //Expose fields in the inspector without being visible to any other script
+    [SerializeField]  GameObject tileViewPrefab;
+    [SerializeField]  GameObject selectorPrefab;
+
+    //Lazy Loading Pattern
+    Transform marker
+    {
+        get
+        {
+            if (_marker==null)
+            {
+                GameObject ins = Instantiate(selectorPrefab) as GameObject;
+                _marker = ins.transform;
+            }
+            return _marker;
+        }
+    }
+    Transform _marker;
+
+    Dictionary<Point, Tile> tiles = new Dictionary<Point, Tile>();
+
+    //Tilemap max ranges
+    [SerializeField] int width = 10;        //refer to X units
+    [SerializeField] int depth = 10;        //refer to Z units
+    [SerializeField] int height = 8;        //refer to step units 
+
+    //Individual modifications
+    [SerializeField] Point pos;
+
+    //Previously saved boards
+    [SerializeField] LevelData levelData;
+
+    //Grow and Shrink actions triggered by Inspector
+    public void GrowArea()
+    {
+        Rect r = RandomRect();
+        GrowRect(r);
+    }
+    public void ShrinkArea()
+    {
+        Rect r = RandomRect();
+        ShrinkRect(r);
+    }
+
+    //Detail actions
+    Rect RandomRect()
+    {
+        int x = Random.Range(0, width);
+        int y = Random.Range(0, depth);
+        int w = Random.Range(1, width - x + 1);
+        int h = Random.Range(1, depth - y + 1);
+        return new Rect(x, y, w, h);
+    }
+    void GrowRect(Rect rect)
+    {
+        for (int y = (int)rect.yMin; y < (int)rect.yMax; ++y)
+        {
+            for (int x = (int)rect.xMin; x < (int)rect.xMax; ++x)
+            {
+                Point p = new Point(x, y);
+                GrowSingle(p);
+            }
+        }
+    }
+    void ShrinkRect(Rect rect)
+    {
+        for (int y = (int)rect.yMin; y < (int)rect.yMax; ++y)
+        {
+            for (int x = (int)rect.xMin; x < (int)rect.xMax; ++x)
+            {
+                Point p = new Point(x, y);
+                ShrinkSingle(p);
+            }
+        }
+    }
+    Tile Create()
+    {
+        GameObject instance = Instantiate(tileViewPrefab) as GameObject;
+        instance.transform.parent = transform;
+        return instance.GetComponent<Tile>();
+    }
+    Tile GetOrCreate(Point p)
+    {
+        if (tiles.ContainsKey(p))
+            return tiles[p];
+
+        Tile t = Create();
+        t.Load(p, 0);
+        tiles.Add(p, t);
+
+        return t;
+    }
+    void GrowSingle(Point p)
+    {
+        Tile t = GetOrCreate(p);
+        if (t.height < height)
+            t.Grow();
+    }
+    void ShrinkSingle(Point p)
+    {
+        if (!tiles.ContainsKey(p))
+            return;
+
+        Tile t = tiles[p];
+        t.Shrink();
+
+        if (t.height <= 0)
+        {
+            tiles.Remove(p);
+            DestroyImmediate(t.gameObject);
+        }
+    }
+
+    //Grow or Shrink single tile by hand
+    public void Grow()
+    {
+        GrowSingle(pos);
+    }
+    public void Shrink()
+    {
+        ShrinkSingle(pos);
+    }
+
+    //Update selector pos
+    public void UpdateMarker()
+    {
+        Tile t = tiles.ContainsKey(pos) ? tiles[pos] : null;
+        marker.localPosition = t != null ? t.center : new Vector3(pos.x, 0, pos.y);
+    }
+
+    //Clear the existing board
+    public void Clear()
+    {
+        for (int i = transform.childCount - 1; i >= 0; --i)
+            DestroyImmediate(transform.GetChild(i).gameObject);
+        tiles.Clear();
+    }
+
+    //Save the current map data
+    public void Save()
+    {
+        string filePath = Application.dataPath + "/Resources/Levels";
+        if (!Directory.Exists(filePath))
+        {
+            AssetDatabase.CreateFolder("Assets/Resources", "Levels");
+            AssetDatabase.Refresh();
+        }
+
+        LevelData board = ScriptableObject.CreateInstance<LevelData>();
+        board.tiles = new List<Vector3>(tiles.Count);
+        foreach (Tile tile in tiles.Values)
+            board.tiles.Add(new Vector3(tile.pos.x, tile.height, tile.pos.y));
+
+        string fileName = string.Format("Assets/Resources/Levels/{1}.asset", filePath, name);
+        AssetDatabase.CreateAsset(board, fileName);
+    }
+
+    //Load saved map file data
+    public void Load()
+    {
+        Clear();
+        if (!levelData)
+            return;
+
+        foreach (Vector3 v in levelData.tiles)
+        {
+            Tile t = Create();
+            t.Load(v);
+            tiles.Add(t.pos, t);
+        }
+    }
+}
