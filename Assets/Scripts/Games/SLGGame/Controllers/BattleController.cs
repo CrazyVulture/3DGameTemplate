4565aad91d7f8c7d6534e96afd63cef515f4198e
CrazyVulture <gaochen010@sina.com>
2019-03-21 16:22:32 +0800

    create state infrasturcture

---
+++ Assets/Scripts/Games/SLGGame/Controllers/BattleController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BattleController : StateMachine
+{
+    public CamereRig camereRig;
+    public Board board;
+    public LevelData levelData;
+    public Transform selector;
+    public Point pos;
+
+    void Start()
+    {
+        ChangeState<InitBattleState>();
+    }
+}
---
+++ Assets/Scripts/Games/SLGGame/ViewModel/Board.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] GameObject tilePrefab;
+    public Dictionary<Point, Tile> tiles = new Dictionary<Point, Tile>();
+
+    public void Load(LevelData data)
+    {
+        for (int i = 0; i < data.tiles.Count; ++i)
+        {
+            GameObject ins = Instantiate(tilePrefab) as GameObject;
+            Tile t = ins.GetComponent<Tile>();
+            t.Load(data.tiles[i]);
+            tiles.Add(t.pos, t);
+        }
+    }
+}
