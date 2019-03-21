bc3cb06e88e3a9d5167abbdae020d48936d951ac
CrazyVulture <gaochen010@sina.com>
2019-03-21 11:36:39 +0800

    add input events

---
+++ Assets/Scripts/Common/EventArgs/InfoEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class InfoEventArgs<T> : EventArgs
+{
+    public T info;
+
+    public InfoEventArgs()
+    {
+        info = default;
+    }
+
+    public InfoEventArgs(T info)
+    {
+        this.info = info;
+    }
+}
---
+++ Assets/Scripts/Games/SLGGame/ControlTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ControlTest : MonoBehaviour
+{
+
+    void OnEnable()
+    {
+        InputController.moveEvent += OnMoveEvent;
+        InputController.fireEvent += OnFireEvent;
+    }
+
+    void OnDisable()
+    {
+        InputController.moveEvent -= OnMoveEvent;
+        InputController.fireEvent -= OnFireEvent;
+    }
+
+    void OnMoveEvent(object sender, InfoEventArgs<Point> e)
+    {
+        Debug.Log("Move " + e.info);
+    }
+
+    void OnFireEvent(object sender, InfoEventArgs<int> e)
+    {
+        Debug.Log("Fire " + e.info.ToString());
+    }
+
+    
+
+    
+}
